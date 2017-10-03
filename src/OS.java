561edf550cefb913565190afbd235ec4c774c59c
Jason Means <means41@live.marshall.edu>
2017-10-03 10:57:59 -0400

    Add classes

---
+++ src/OS.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class OS {
+    public CPU cpu;
+    //public IOdevice io;
+    public boolean isCPUAvailable;
+    //public ProcessTable process_Table;
+    public ArrayList<Process> New_Queue;
+    public ArrayList<Process> Ready_Queue;
+    public ArrayList<Process> Wait_Queue;
+    public ArrayList<Process> Terminated_Queue;
+    //Read the txt input file, for each line,create a process and record its arrival time
+    //Put each process in New_Q queue initially then put them in Ready_Q
+    //Always check whether the CPU is idle or not; if yes, use your scheduler algorithm to select a process from the Ready_Queue for CPU execution
+    //According to the return value of CPU execute(), put the process into the corresponding queue.
+    //Record the time of every operation for computing your latency and response
+}
---
+++ src/PCB.java
@@ -0,0 +1,7 @@
+public class PCB {
+    //To do: PCB data structure of a process
+    //for example: Process_id, Arrive_time, state, PositionOfNextInstructionToExecute(PC value)and so on
+    String id;
+    double arriveTime;
+    int next;
+}
