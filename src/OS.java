2980689a133524b3402f4c9e5598f64ae0a2c753
Jason Means <means41@live.marshall.edu>
2017-10-05 10:50:07 -0400

    Items in new queue are now converted to processes in ready queue, user interface added

--- src/OS.java
+++ src/OS.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.regexp.internal.RE;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -11,11 +13,12 @@ public class OS {
         boolean isCPUAvailable;
         //public ProcessTable process_Table;
         ArrayList<PCB> New_Queue = new ArrayList<>();
-        ArrayList<Process> Ready_Queue;
+        ArrayList<Process> Ready_Queue = new ArrayList<>();
         ArrayList<Process> Wait_Queue;
         ArrayList<Process> Terminated_Queue;
 
-        File f = new File("test");
+        //Read text file and convert lines to PCB objects, add those PCB objects to New_Queue
+        File f = new File("C:\\Users\\jason\\IdeaProjects\\CpuScheduler\\src\\test");
         try {
             BufferedReader bf = new BufferedReader(new FileReader(f));
             Scanner scan = new Scanner(bf);
@@ -24,6 +27,32 @@ public class OS {
                 New_Queue.add(new PCB(line[0], Integer.parseInt(line[1]), Integer.parseInt(line[2]),line[3]));
             }
         } catch (Exception e){}
+
+        //Convert PCB objects to Process objects and add those objects to Reader_Queue
+        for(int i = 0; i<New_Queue.size();i++){
+            Ready_Queue.add(new Process(New_Queue.get(i)));
+        }
+
+        //Give user choice of sorting algorithm
+        Scanner uScan = new Scanner(System.in);
+        boolean done = false;
+        while(!done) {
+            System.out.println("Which sorting algorithm should be used?");
+            System.out.println("1. FCFS");
+            System.out.println("2. Round Robin");
+            System.out.println("3. Last one");
+            int choice = uScan.nextInt();
+            if (choice == 1) {
+                done = true;
+                //ToDo: code for FCFS alg
+            } else if (choice == 2) {
+                done = true;
+                //ToDo: Code for RR alg
+            } else if (choice == 3) {
+                done = true;
+                //ToDo: Code for last alg
+            } else System.out.println("Invalid input");
+        }
         //ToDo: OS class
         //Read the txt input file, for each line,create a process and record its arrival time
         //Put each process in New_Q queue initially then put them in Ready_Q
--- src/Process.java
+++ src/Process.java
@@ -2,10 +2,17 @@ public class Process {
     public PCB Pcb_data;
     public String code; // = CPU-I/O burst Sequence;
     //ToDo: other variables help you computing the latency, response
-    public Process(String process) {
-
+    public Process(PCB p) {
+        this.Pcb_data = p;
+        code = p.getCode();
         //ToDo: Set PCB data, code and others. Set state as "NEW";
     }
 
+    public PCB getPcb_data() {
+        return Pcb_data;
+    }
 
+    public String getCode() {
+        return code;
+    }
 }
