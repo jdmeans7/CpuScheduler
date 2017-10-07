a0c40ce4097cec7d9f47374e0714b754452855b1
amayzinone <32047365+amayzinone@users.noreply.github.com>
2017-10-06 22:45:22 -0400

    Process class update
    edit how you think is more efficient

--- src/Process.java
+++ src/Process.java
@@ -1,36 +1,78 @@
-public class Process {
+public class Process implements Comparable<Process>{
     public PCB Pcb_data;
     public String code; // = CPU-I/O burst Sequence;
+    double timeCreated;
+    int priority;
+    String state;
     public int[] codeA;
-    //ToDo: other variables help you computing the latency, response
-    public Process(PCB p) {
-        this.Pcb_data = p;
-        code = p.getCode();
-        codeA = new int[code.length()];
-        for(int i = 0;i<code.length();i++){
+    int count;
+    
+    //to do: other variables help you computing the latency, response
+    public Process(PCB process) {
+    //set PCB data, code and others
+    	this.code = process.getCode();
+    	this.Pcb_data = process;
+    	this.timeCreated = System.currentTimeMillis();
+    //set state as "NEW";
+    	this.state = process.getState();
+    	this.priority = process.getPriority();
+    	codeA = new int[code.length()];
+    	for(int i = 0;i<code.length();i++){
             codeA[i] = Integer.parseInt((code.substring(i,i+1)));
         }
     }
 
-    public int getBurstNum(){
-        return codeA[Pcb_data.getNext()];
+    public String getState() {
+        return state;
     }
-
-    //setBurstNum is used solely to set the number in the int array of code to a lower number if the time slice runs out and
-    //bubble sort did not run the amount of times it was supposed to.
-    public void setBurstNum(int a){
-        codeA[Pcb_data.getNext()] = a;
+    
+    public void setState(String state) {
+        this.state = state;
+    }
+    
+    public int getBurstNum(){
+        return codeA[count];
+    }
+    
+    public void nextBurstNum(){
+    	count++;
+    }
+    
+    public int getCount(){
+    	return count;
+    }
+    
+    public void setBurstNum(int i){
+    	codeA[count] = i; 
     }
 
     public int[] getCodeA() {
         return codeA;
+    }
+    
+    public double getTimeCreated() {
+        return timeCreated;
+    }
+
+    public String getCode() {
+        return code;
+    }
+    
+    public int getPriority(){
+    	return priority;
     }
 
     public PCB getPcb_data() {
         return Pcb_data;
     }
 
-    public String getCode() {
-        return code;
-    }
+	@Override
+	public int compareTo(Process o) {
+		// TODO Auto-generated method stub
+		int comparePriority = o.getPriority();
+		//ascending return this.priority - comparePriority; 
+		//descending
+		return comparePriority - this.priority;
+	}
+    
 }
