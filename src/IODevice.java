71770c8b4f0a42752de4a1b7355718306056ce29
amayzinone <32047365+amayzinone@users.noreply.github.com>
2017-10-09 16:44:09 -0400

    Updateio

--- src/IODevice.java
+++ src/IODevice.java
@@ -1,30 +1,57 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-public class IODevice {
+public class IODevice implements Runnable {
 	
-    public IODevice(ArrayList<Process> Wait_Queue) {}
+	Process currentProcess;
+	String currentState;
+	private String threadName;
+    private Thread t;
+	
+    public IODevice(ArrayList<Process> Wait_Queue, String thread) {
+    	threadName = thread;
+    }
 
     public boolean BusyOrNot;
     int burstAmount;
     
-    //Always pick one process from Wait_Queue to execute
-    public String execute(Process p) {
-        BusyOrNot = true;
-        burstAmount = p.getBurstNum();
+    public void setCurretProcess(Process p){
+    	currentProcess = p;
+    }
+    
+    public String getCurrentState(){
+    	return currentState;
+    }
+    
+    public boolean isIOBusy(){
+    	return BusyOrNot;
+    }
+    
+    public void execute(){
+    	burstAmount = currentProcess.getBurstNum();
         for(int i = 0; i<burstAmount;i++){ //p.getioburstnumber
             bubbleSort();
         }
-        p.nextBurstNum();
+        currentProcess.nextBurstNum();
         //CallBubble Sort()for IO_bursttimes and then return “ready”
-        return "ready";
+        currentState = "ready";
+    }
+    
+    //Always pick one process from Wait_Queue to execute
+    @Override
+	public void run() {
+    	try{
+	        execute();
+    	}catch(Exception e){
+    	}
     }
 
     public void bubbleSort() {
+    	BusyOrNot = true;
         Random rand = new Random();
         int[] arr = new int[10];
         for(int i = 0;i<arr.length;i++){
-            arr[i] = rand.nextInt(100);
+            arr[i] = rand.nextInt(1000);
         }
         int n = arr.length;
         for (int i = 0; i < n-1; i++)
@@ -36,5 +63,14 @@ public class IODevice {
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
+    	BusyOrNot = false;
     }
+
+    public void start () {
+	      System.out.println("Starting " +  threadName );
+	      if (t == null) {
+	         t = new Thread (this, threadName);
+	         t.start ();
+	      }
+	   }
 }
